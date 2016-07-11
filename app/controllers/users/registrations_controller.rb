af84a3742f0dece4f5cf9f04c2fccaeab312931d
richardbota <richard.bota@khk.ee>
2016-07-11 10:35:02 +0000

    Added devise for stripe

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,17 @@
+class Users::RegistrationsController < Devise::RegistrationsController
+  before_filter :select_plan, only: :new
+  
+  def create
+    super do |resource|
+      if params[:plan]
+        resource.plan_id = params[:plan]
+        if resource.plan_id == 2
+          resource.save_with_payment
+        else
+          resource.save
+        end
+      end
+    end
+  end
+
+end
